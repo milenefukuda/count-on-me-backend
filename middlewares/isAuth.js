0305d7e1f3b8640d25581aa9d31b2ad1424ad7bf
Milene Fukuda <milene.fukuda@gmail.com>
2023-05-12 18:40:43 +0100

    isAuth middlewares

---
+++ middlewares/isAuth.js
@@ -0,0 +1,8 @@
+// verifica o autor da requisição
+
+const { expressjwt } = require("express-jwt");
+
+export default expressjwt({
+  secret: process.env.TOKEN_SIGN_SECRET,
+  algorithms: ["HS256"],
+});
