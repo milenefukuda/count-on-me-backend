2c2d374919756c074cf04b8ada5ef88f63b1cfa4
Milene Fukuda <milene.fukuda@gmail.com>
2023-05-17 18:40:24 +0100

    event routes

--- routes/event.routes.js
+++ routes/event.routes.js
@@ -1,5 +1,8 @@
 import express from "express";
 import { EventModel } from "../models/event.model.js";
+import { UserModel } from "../models/user.model.js";
+import isAuth from "../middlewares/isAuth.js";
+import attachCurrentUser from "../middlewares/attachCurrentUser.js";
 
 const eventRouter = express.Router();
 
@@ -22,5 +25,52 @@ eventRouter.get("/event/:id", async (req, res) => {
     return res.status(500).json({ message: "Something went wrong" });
   }
 });
+
+// Criar novo evento
+eventRouter.post("/create", isAuth, attachCurrentUser, async (req, res) => {
+  try {
+    const newEvent = await EventModel.create({
+        ...req.body,
+        creator: req.currentUser._id,
+      }),
+      updatedUser = await UserModel.findByIdAndUpdate(req.currentUser._id, {
+        $push: { events: newEvent._doc._id },
+      });
+    return res.status(201).json(newEvent);
+  } catch (err) {
+    console.log(err);
+    return res.status(500).json(err);
+  }
+});
+
+// Apagar um evento
+eventRouter.delete(
+  "/delete/:eventId",
+  isAuth,
+  attachCurrentUser,
+  async (req, res) => {
+    try {
+      const { eventId } = req.params,
+        selEvent = await EventModel.findById(eventId);
+      if (
+        JSON.stringify(selEvent._doc.creator) !=
+        JSON.stringify(req.currentUser._id)
+      ) {
+        return res.status(401).json("Not authorized");
+      }
+      const deletedEvent = await EventModel.findByIdAndDelete(eventId);
+      const updatedUser = await UserModel.findByIdAndUpdate(
+        req.currentUser._id,
+        {
+          $pull: { events: deletedEvent._doc._id },
+        }
+      );
+      return res.status(200).json(deletedEvent);
+    } catch (err) {
+      console.log(err);
+      return res.status(500).json(err);
+    }
+  }
+);
 
 export default eventRouter;
